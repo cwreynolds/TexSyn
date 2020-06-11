f1a2119de6132576cf83d7c2a74f416b3613e943
Craig Reynolds <cwr@red3d.com>
2020-06-11 11:26:56 -0700

    Add grabPrintLock(), use it in debugPrint().
    Keep 2002 version as “debugPrintNL()” for old times sake.

--- Utilities.h
+++ Utilities.h
@@ -19,7 +19,14 @@ class Vec2;
 
 // for debugging: prints one line with a given C expression, an equals sign,
 // and the value of the expression.  For example "angle = 35.6"
-#define debugPrint(e) (std::cout << #e" = " << (e) << std::endl << std::flush)
+#define debugPrint(e) { grabPrintLock() debugPrintNL(e); }
+
+// Original (circa 2002) "non locking" version, in case it is ever useful.
+#define debugPrintNL(e) (std::cout << #e" = " << (e) << std::endl << std::flush)
+
+// Global mutex to allow synchronizing console output from parallel threads
+static std::mutex print_mutex_;
+#define grabPrintLock() std::lock_guard<std::mutex> print_lock_(print_mutex_);
 
 // Just for convenience in hand-written prototypes and tests:
 const float pi = M_PI;
