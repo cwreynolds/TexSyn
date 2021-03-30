4e77db7b2663273be18a6c43c3e3d79d27bee0eb
Craig Reynolds <cwr@red3d.com>
2021-03-30 16:28:00 -0700

    WIP experiment: PhasorNoisePrototype

--- Operators.h
+++ Operators.h
@@ -1671,3 +1671,217 @@ private:
     std::vector<Disk> disks_;  // TODO should this be const?
     std::shared_ptr<DiskOccupancyGrid> disk_occupancy_grid_;
 };
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/*
+
+// Source for "Procedural phasor noise" https://www.shadertoy.com/view/wlsXWf
+
+
+// TODO WIP prototype, perhaps to be replaced with "Procedural phasor noise"?
+class PhasorNoisePrototype : public Texture
+{
+public:
+    PhasorNoisePrototype(const Texture& texture0,
+                         const Texture& texture1)
+      : texture0_(texture0),
+        texture1_(texture1) {}
+    
+    // TODO code below originally copied from https://www.shadertoy.com/view/wlsXWf
+    
+    //phasor noise parameters
+    float _f = 50.0;
+    float _b = 32.0;
+    float _o = 1.0;
+    float _kr;
+    int _impPerKernel = 16;
+    int _seed = 1;
+    
+//    vec2 uv;
+    Vec2 uv;
+
+    
+    ///////////////////////////////////////////////
+    //prng
+    ///////////////////////////////////////////////
+    
+    int N = 15487469;
+    int x_;
+    void seed(int s){x_ = s;}
+    int next() { x_ *= 3039177861; x_ = x_ % N;return x_; }
+    float uni_0_1() {return  float(next()) / float(N);}
+    float uni(float min, float max){ return min + (uni_0_1() * (max - min));}
+    
+    
+    int morton(int x, int y)
+    {
+        int z = 0;
+        for (int i = 0 ; i < 32* 4 ; i++) {
+            z |= ((x & (1 << i)) << i) | ((y & (1 << i)) << (i + 1));
+        }
+        return z;
+    }
+    
+    
+    
+//    vec2 phasor(vec2 x, float f, float b, float o, float phi)
+    Vec2 phasor(Vec2 x, float f, float b, float o, float phi)
+    {
+        
+//        float a = exp(-M_PI * (b * b) * ((x.x * x.x) + (x.y * x.y)));
+//        float s = sin (2.0* M_PI * f  * (x.x*cos(o) + x.y*sin(o))+phi);
+//        float c = cos (2.0* M_PI * f  * (x.x*cos(o) + x.y*sin(o))+phi);
+//        return vec2(a*c,a*s);
+        float a = exp(-pi * (b * b) * ((x.x() * x.x()) + (x.y() * x.y())));
+        float s = sin (2.0* pi * f  * (x.x()*cos(o) + x.y()*sin(o))+phi);
+        float c = cos (2.0* pi * f  * (x.x()*cos(o) + x.y()*sin(o))+phi);
+        return Vec2(a*c,a*s);
+    }
+    
+    
+    
+    void init_noise()
+    {
+//        _kr = sqrt(-log(0.05) / M_PI) / _b;
+        _kr = sqrt(-log(0.05) / pi) / _b;
+    }
+    
+    // TODO I think this comes from the Shadertoy framework, trying to mock...
+    Vec2 iResolution = Vec2(1280, 720);
+
+    
+    // TODO note: substituting Vec2 for ivec2 -- any issue?
+//    vec2 cell(ivec2 ij, vec2 uv, float f, float b)
+    Vec2 cell(Vec2 ij, Vec2 uv, float f, float b)
+    {
+//        int s= morton(ij.x,ij.y) + 333;
+        int s= morton(ij.x(), ij.y()) + 333;
+        s = s==0? 1: s +_seed;
+        seed(s);
+        int impulse  =0;
+        int nImpulse = _impPerKernel;
+        float  cellsz = 2.0 * _kr;
+//        vec2 noise = vec2(0.0);
+        Vec2 noise;
+        while (impulse <= nImpulse)
+        {
+//            vec2 impulse_centre = vec2(uni_0_1(),uni_0_1());
+//            vec2 d = (uv - impulse_centre) *cellsz;
+//            float rp = uni(0.0,2.0*M_PI) ;
+            Vec2 impulse_centre = Vec2(uni_0_1(),uni_0_1());
+            Vec2 d = (uv - impulse_centre) *cellsz;
+            float rp = uni(0.0,2.0*pi) ;
+            
+            // TODO this is bound to be wrong
+//            vec2 trueUv = ((vec2(ij) + impulse_centre) *cellsz) *  iResolution.yy / iResolution.xy;
+//            trueUv.y = -trueUv.y;
+
+            
+            Vec2 cwr_temp = (ij + impulse_centre) *cellsz;
+            float cwr_aspect = iResolution.x() / iResolution.y();
+            Vec2 trueUv(cwr_temp.x() * cwr_aspect, -cwr_temp.y() * cwr_aspect);
+            
+            
+//            float o = texture(iChannel0, trueUv).x *2.0* M_PI;
+            float o = texture(iChannel0, trueUv).x *2.0* pi;
+            noise += phasor(d, f, b ,o,rp );
+            impulse++;
+        }
+        return noise;
+    }
+    
+    
+//    vec2 eval_noise(vec2 uv, float f, float b)
+//    {
+//        float cellsz = 2.0 *_kr;
+//        vec2 _ij = uv / cellsz;
+//        ivec2  ij = ivec2(_ij);
+//        vec2  fij = _ij - vec2(ij);
+//        vec2 noise = vec2(0.0);
+//        for (int j = -2; j <= 2; j++) {
+//            for (int i = -2; i <= 2; i++) {
+//                ivec2 nij = ivec2(i, j);
+//                noise += cell(ij + nij , fij - vec2(nij),f,b);
+//            }
+//        }
+//        return noise;
+//    }
+    Vec2 eval_noise(Vec2 uv, float f, float b)
+    {
+        float cellsz = 2.0 *_kr;
+        Vec2 _ij = uv / cellsz;
+        // TODO ignored use of ivec2, was that being used as floor?
+        Vec2 ij = _ij;
+        Vec2 fij = _ij - ij;
+        Vec2 noise;
+        for (int j = -2; j <= 2; j++) {
+            for (int i = -2; i <= 2; i++) {
+                ivec2 nij = ivec2(i, j);
+                noise += cell(ij + nij , fij - vec2(nij),f,b);
+            }
+        }
+        return noise;
+    }
+
+    float PWM(float x, float r)
+    {
+//        return mod(x,2.0*M_PI)> 2.0*M_PI *r ? 1.0 : 0.0;
+        return std::fmod(x,2.0*pi)> 2.0*pi *r ? 1.0 : 0.0;
+    }
+    
+    float square(float x)
+    {
+        return PWM(x,0.5);
+    }
+    
+    float sawTooth(float x)
+    {
+//        return mod(x,2.0*M_PI)/(2.0*M_PI);
+        return std::fmod(x,2.0*pi)/(2.0*pi);
+    }
+    
+//    vec3 hsv2rgb(vec3 c)
+//    {
+//        vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+//        vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+//        return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+//    }
+    
+    void mainImage( out vec4 fragColor, in vec2 fragCoord )
+    {
+        uv = fragCoord/iResolution.y;
+        uv.y=-uv.y;
+        init_noise();
+//        float o = iMouse.x/iResolution.x * 2.0*M_PI;
+        float o = iMouse.x/iResolution.x * 2.0*pi;
+        vec2 phasorNoise = eval_noise(uv,_f,_b);
+        vec2 dir = vec2(cos(o),sin(o));
+        float phi = atan(phasorNoise.y,phasorNoise.x);
+        float I = length(phasorNoise);
+        float angle = texture(iChannel0,fragCoord/iResolution.xy ).x;
+        float p1 = PWM(phi, uv.x+0.2 *0.5);
+        float g1 = exp(-(uv.x-0.3)*(uv.x-0.3)*20.0);
+        float p2 = sawTooth(phi);
+        float g2 = exp(-(uv.x-0.9)*(uv.x-0.9)*20.0);
+//        float p3 = sin(phi+M_PI)+0.5*0.5;
+        float p3 = sin(phi+pi)+0.5*0.5;
+        float g3 = exp(-(uv.x-1.5)*(uv.x-1.5)*20.0);
+        vec3 phasorfield  = vec3(sin(phi)*0.3 +0.5);
+        
+        float profile =p1*g1+p2*g2+p3*g3;
+        float sumGaus= g1+g2+g3;
+        
+        phasorfield = vec3(profile/sumGaus);
+        fragColor = vec4(phasorfield,1.0);
+    }
+
+    
+private:
+    const Texture& texture0_;
+    const Texture& texture1_;
+};
+ 
+ */
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
