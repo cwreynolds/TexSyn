d2ddb2e12bbd4cb62c54d18f2f532947789a4094
Craig Reynolds <cwr@red3d.com>
2021-06-21 16:36:57 -0700

    WIP refactor Color for HSV subclass.

--- Color.h
+++ Color.h
@@ -9,12 +9,6 @@
 #pragma once
 #include "Utilities.h"
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// TODO expermental
-// Helper class to represent a color in hue-saturation-value space.
-class HSV;
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 // TODO move elsewhere
 // Simple class to represent color as RGB float values on [0, 1]
 class Color
@@ -30,6 +24,7 @@ public:
         blue_(paper_over_abnormal_values(b)) {}
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // Convert from HSV to RGB (defined below, after HSV class).
+    class HSV;
     Color(HSV hsv);
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // assignment, set RGB components
@@ -47,35 +42,53 @@ public:
     bool operator!=(const Color& c) const { return !(*this == c); }
     // Luma (relative luminance?) see https://en.wikipedia.org/wiki/Luma_(video)
     float luminance() const {return 0.2126 * r() + 0.7152 * g() + 0.0722 * b();}
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // TODO still thinking about this API and these names:
+    // TODO obsolete? -- should be once _old operators removed.
     // Set this color to the one described by the given HSV values.
     void setHSV(float h, float s, float v);
+    // TODO obsolete? -- should be once _old operators removed.
     // Get the HSV values for this color. Returned by setting non-const refs.
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // TODO obsolete?
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     void getHSV(float& h, float& s, float& v) const;
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // Get the HSV values for a Color. Returned as 3-tuple of floats.
     // TODO needed? Wrote first version of this before the HSV helper class.
     std::tuple<float, float, float> getHSV() const;
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // TODO obsolete? -- should be once _old operators removed.
     // Return a Color made from the given HSV values
     static Color makeHSV(float h, float s, float v);
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // Get H, S, or V components of this Color.
     float getH() const;
     float getS() const;
     float getV() const;
-    // Static utilities to convert RGB→HSV and HSV→RGB
-    static void convertRGBtoHSV(float red, float green, float blue,
-                                float& H, float& S, float& V);
-    static void convertHSVtoRGB(float h, float s, float v,
-                                float& R, float& G, float& B);
-    // Inline operators: + - * / += *=
-    Color operator+(Color v) const;
-    Color operator-(Color v) const;
-    Color operator*(Color c) const;  // color * color, aka "tint"
-    Color operator*(float s) const;
+    
+    
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
+    // Inline operators: + - *
+    Color operator+(Color v) const
+    {
+        return Color(r() + v.r(), g() + v.g(), b() + v.b());
+    }
+    Color operator-(Color v) const
+    {
+        return Color(r() - v.r(), g() - v.g(), b() - v.b());
+    }
+    Color operator*(Color c) const  // color * color, aka "tint"
+    {
+        return Color(r() * c.r(), g() * c.g(), b() * c.b());
+    }
+    Color operator*(float s) const
+    {
+        return Color(r() * s, g() * s, b() * s);
+    }
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    
+    
     Color operator/(float s) const { return *this * (1 / s); };
     Color operator+=(const Color& rhs) { return *this = *this + rhs; }
     Color operator*=(float s) { return *this = *this * s; }
@@ -92,18 +105,175 @@ public:
     // Assert RGB components are "normal".
     void assertNormal() const { assert(isNormal()); }
     static Color gray(float value) { return { value, value, value }; }
-    // Accessors:
+    // Accessors for RGB components (long and short name).
     float red() const { return red_; }
     float green() const { return green_; }
     float blue() const { return blue_; }
-    // TODO Are these short names a good idea?
     float r() const { return red_; }
     float g() const { return green_; }
     float b() const { return blue_; }
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Helper class to represent a color in hue-saturation-value space.
+    class HSV
+    {
+    public:
+        // Construct from H, S, and V.  (TODO needed?)
+        HSV(float h, float s, float v) : h_(h), s_(s), v_(v) {}
+        // Construct from (RGB) Color object
+        HSV(Color c) { Color::convertRGBtoHSV(c.r(), c.g(), c.b(), h_, s_, v_); }
+        // Accessors for H, S, and V.
+        float getH() const { return h_; }
+        float getS() const { return s_; }
+        float getV() const { return v_; }
+        float h() const { return h_; }
+        float s() const { return s_; }
+        float v() const { return v_; }
+        // Return a copy of this HSV with one of its components overwritten.
+        HSV newH(float h) const { return {h, s_, v_}; }
+        HSV newS(float s) const { return {h_, s, v_}; }
+        HSV newV(float v) const { return {h_, s_, v}; }
+        // Return H, S, and V as 3-tuple of floats.
+        std::tuple<float, float, float> getHSV() const { return {h_, s_, v_}; }
+    private:
+        float h_ = 0;
+        float s_ = 0;
+        float v_ = 0;
+    };
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 private:
     float red_;
     float green_;
     float blue_;
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // TODO remove this "public:" once the Texture::diff() for "_old" removed
+public:
+    
+    // Transform color space from "Red Green Blue" to "Hue Saturation Value"
+    //
+    // Method due to Alvy Ray Smith, while at NYIT, first published in 1978.
+    // See http://en.wikipedia.org/wiki/HSV_color_space
+    //
+    // Code due to T. Nathan Mundhenk of the USC iLab and generously provided
+    // on the web: http://ilab.usc.edu/wiki/index.php/HSV_And_H2SV_Color_Space
+    //
+    // I made all six parameters (RGBHSV) range on [0 1] and cosmetic changes.
+    //
+    static void convertRGBtoHSV (float red, float green, float blue,
+                                 float& H, float& S, float& V)
+    {
+        // Modified to count and report, but otherwise tolerate, "abnormal" floats.
+        const float R = 255 * paper_over_abnormal_values(red);
+        const float G = 255 * paper_over_abnormal_values(green);
+        const float B = 255 * paper_over_abnormal_values(blue);
+        if((B > G) && (B > R)) // Blue Is the dominant color
+        {
+            V = B; // Value is set as the dominant color
+            if(V != 0)
+            {
+                float min;
+                if(R > G) min = G;
+                else min = R;
+                const float delta = V - min;
+                if(delta != 0)
+                { S = (delta/V); H = 4 + (R - G) / delta; }
+                else
+                { S = 0; H = 4 + (R - G); }
+                H *=60; if(H < 0) H += 360;
+                V =(V/255);
+            }
+            else
+            { S = 0; H = 0;}
+        }
+        else if(G > R) // Green is the dominant color
+        {
+            V = G;
+            if(V != 0)
+            {
+                float min;
+                if(R > B) min = B;
+                else  min = R;
+                const float delta = V - min;
+                if(delta != 0)
+                { S = (delta/V); H = 2 + (B - R) / delta; }
+                else
+                { S = 0; H = 2 + (B - R); }
+                H *=60; if(H < 0) H += 360;
+                V =  (V/255);
+            }
+            else
+            { S = 0; H = 0;}
+        }
+        else // Red is the dominant color
+        {
+            V = R;
+            if(V != 0)
+            {
+                float min;
+                if(G > B) min = B;
+                else  min = G;
+                const float delta = V - min;
+                if(delta != 0)
+                { S = (delta/V); H = (G - B) / delta; }
+                else
+                { S = 0; H = (G - B); }
+                H *=60; if(H < 0) H += 360;
+                V =  (V/255);
+            }
+            else
+            { S = 0; H = 0;}
+        }
+        H /= 360.0f;
+        
+        // Modified to count and report, but otherwise tolerate, "abnormal" floats.
+        H = paper_over_abnormal_values(H);
+        S = paper_over_abnormal_values(S);
+        V = paper_over_abnormal_values(V);
+    }
+    
+    // Transform color space from "Hue Saturation Value" to "Red Green Blue"
+    //
+    // Method due to Alvy Ray Smith, first published in 1978.
+    // See http://en.wikipedia.org/wiki/HSV_color_space
+    //
+    // Code due to T. Nathan Mundhenk of the USC iLab and generously provided
+    // on the web: http://ilab.usc.edu/wiki/index.php/HSV_And_H2SV_Color_Space
+    //
+    // I made all six parameters (RGBHSV) range on [0 1] and cosmetic changes.
+    //
+    // I removed some work-arounds dated "20091119", cleaning them up and adding a
+    // single numeric validity check at the end.
+    //
+    static void convertHSVtoRGB(float h, float s, float v,
+                                float& R, float& G, float& B)
+    {
+        const float H = fmod_floor(h, 1) * 360;
+        const float S = s;
+        const float V = v;
+        if (V == 0)
+            { R = 0; G = 0; B = 0; }
+        else if (S == 0)
+            { R = V; G = V; B = V; }
+        else
+        {
+            const float hf = H / 60.0;
+            int i = std::max(0, std::min(5, int(std::floor(hf))));
+            const float f  = hf - i;
+            const float pv  = V * ( 1 - S );
+            const float qv  = V * ( 1 - S * f );
+            const float tv  = V * ( 1 - S * ( 1 - f ) );
+            switch (i)
+            {
+                case 0: R = V;  G = tv; B = pv; break; // Red is dominant color
+                case 1: R = qv; G = V;  B = pv; break; // Green is dominant color
+                case 2: R = pv; G = V;  B = tv; break;
+                case 3: R = pv; G = qv; B = V;  break; // Blue is dominant color
+                case 4: R = tv; G = pv; B = V;  break;
+                case 5: R = V;  G = pv; B = qv; break; // Red is dominant color
+            }
+        }
+        Color(R, G, B).assertNormal();
+    }
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 };
 
 // Is distance between RGB vectors no more than epsilon?
@@ -113,45 +283,8 @@ bool withinEpsilon(Color a, Color b, float epsilon);
 std::ostream& operator<<(std::ostream& os, const Color& v);
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// TODO expermental
-
-// Helper class to represent a color in hue-saturation-value space.
-class HSV
-{
-public:
-    // Construct from H, S, and V.  (TODO needed?)
-    HSV(float h, float s, float v) : h_(h), s_(s), v_(v) {}
-    // Construct from (RGB) Color object
-    HSV(Color c) { Color::convertRGBtoHSV(c.r(), c.g(), c.b(), h_, s_, v_); }
-    // Accessors for H, S, and V.
-    float getH() const { return h_; }
-    float getS() const { return s_; }
-    float getV() const { return v_; }
-    //~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-    
-    // TODO experimental
-    
-    float h() const { return h_; }
-    float s() const { return s_; }
-    float v() const { return v_; }
-
-    
-    // Return a copy of this HSV with one of its components overwritten.
-    HSV newH(float h) const { return {h, s_, v_}; }
-    HSV newS(float s) const { return {h_, s, v_}; }
-    HSV newV(float v) const { return {h_, s_, v}; }
-
-    //~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-    // Return H, S, and V as 3-tuple of floats.
-    std::tuple<float, float, float> getHSV() const { return {h_, s_, v_}; }
-//    // Return (RGB) Color version of this HSV color
-//    Color getColor() const { return Color(*this); }
-private:
-    float h_ = 0;
-    float s_ = 0;
-    float v_ = 0;
-};
+// Hoist the internal helper class to global scope.
+typedef Color::HSV HSV;
 
 // Construct an RGB Color from an HSV instance.
 inline Color::Color(HSV h)
